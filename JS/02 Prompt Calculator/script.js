dddd435bd428d997e7fe74afba8e25873326907e
RoseTechInstitute <rosetechlyari@gmail.com>
2024-11-30 17:13:43 +0500

    function in JS

--- JS/02 Prompt Calculator/script.js
+++
@@ -1,4 +0,0 @@
-let num1 = prompt("write your first number")
-let num2 = prompt("write your second number")
-
-alert(num1+num2)
---
+++ JS/03 Functions In JS/script.js
@@ -0,0 +1,21 @@
+// let num1 = prompt("write your first number")
+// let num2 = prompt("write your second number")
+
+// console.log(typeof(num1));
+
+// num1 = Number(num1)
+// num2 = Number(num2)
+
+// alert(num1+num2)
+
+function myfun() {
+  let num1 = prompt("write your first number");
+  let num2 = prompt("write your second number");
+
+  console.log(typeof num1);
+
+  num1 = Number(num1);
+  num2 = Number(num2);
+
+  alert(num1 + num2);
+}
