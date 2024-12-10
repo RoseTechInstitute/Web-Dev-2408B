cc10adda9dd168e1bf70a4e8120eb44af020047c
RafayMemon01 <rafayrashid200611@gmail.com>
2024-12-10 18:14:34 +0500

    if else

--- JS/03 Functions In JS/script.js
+++ JS/03 Functions In JS/script.js
@@ -12,7 +12,7 @@ function myfun() {
   let num1 = prompt("write your first number");
   let num2 = prompt("write your second number");
 
-  console.log(typeof num1);
+  console.log(typeof(num1));
 
   num1 = Number(num1);
   num2 = Number(num2);
--- JS/04 If Else/index.js
+++ JS/04 If Else/index.js
@@ -1,4 +1,19 @@
-let num1 = 1;
-let num2 = 2;
+// let num1 = 2;
+// let num2 = 2;
 
-console.log(num1+num2)
+// // console.log(num1+num2)
+
+// if(num1 === 1){
+//     console.log("Yes num1 is = to 1")
+// }else{
+//     console.log("No, Num1 is Not Equal to 1")
+// }
+
+
+let userInput = "lahore"  //prompt("What is your city name?")
+
+if(userInput === "karachi"){
+    console.log("Yes, we are neighbors")
+}else{
+    console.log("no we are not neighbors")
+}
