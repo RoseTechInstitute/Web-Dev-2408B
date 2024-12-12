a91a13c843520331f5ef29d79bf711e0fc5b9f97
RafayMemon01 <rafayrashid200611@gmail.com>
2024-12-12 17:17:03 +0500

    weather suggestion

---
+++ JS/05 Get input from form/app.js
@@ -0,0 +1,16 @@
+const weatherCheck = (event) =>{
+    event.preventDefault()
+
+    // console.log(event)
+
+    let userCity = document.getElementById("userCity").value;
+    console.log(userCity)
+    if(userCity === "karachi"){
+        console.log("Today weather is Cold! Take Your Jacket on")
+    } else if(userCity === "hydrabad"){
+        console.log("Today weather is Cold! Take Your Jacket on and avoid night outing!")
+    }
+    else{
+        console.log("No Data Found, Sorry We have no enough data for your city")
+    }
+}
