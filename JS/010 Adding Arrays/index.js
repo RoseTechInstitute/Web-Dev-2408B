983971600758db3f0998f261045ec8dfda94ae02
RoseTechInstitute <rosetechlyari@gmail.com>
2025-01-02 18:09:35 +0500

    class code array

---
+++ JS/010 Adding Arrays/index.js
@@ -0,0 +1,12 @@
+let cities = []
+
+cities[0] = "karachi"
+cities[1] = "lahore"
+cities[5] = "islamabad"
+console.log("your array is: ",cities)
+
+// cities.pop()
+cities.push("multan","quetta")
+
+console.log("your array is: ",cities)
+console.log("your array length is: ",cities.length)
