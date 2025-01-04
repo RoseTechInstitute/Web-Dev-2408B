4200c6dfd080531607d7363d500b9eb00fd94e52
RoseTechInstitute <rosetechlyari@gmail.com>
2025-01-04 18:23:20 +0500

    array non premetive behavior

---
+++ JS/011 Inserting and Removing Arrays/index.js
@@ -0,0 +1,32 @@
+let animals = ["lion", "elephent", "fox", "monkey", "leopard", "goat"]
+
+// animals.shift()
+
+// animals.unshift("lion","dog","horse")
+
+// console.log(animals)
+
+// animals.splice(2,2,"cat")
+
+// console.log(animals)
+
+
+//Softcopy
+
+// let newAnimals = animals
+
+// animals.pop()
+
+// console.log(animals)
+// console.log(newAnimals)
+
+// hardCopy
+
+// let newAnimals = [...animals]
+
+// let newAnimals = animals.slice()
+
+// animals.pop()
+
+// console.log(animals)
+// console.log(newAnimals)
