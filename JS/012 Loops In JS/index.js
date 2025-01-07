32593c6b8ad4e5fd2f629cc45e01649c699fc570
RoseTechInstitute <rosetechlyari@gmail.com>
2025-01-07 17:47:27 +0500

    whiile and do while loop

--- JS/012 Loops In JS/index.js
+++ JS/012 Loops In JS/index.js
@@ -1,32 +1,56 @@
-const cleanestCities = [
-    "tokyo",
-    "singapore",
-    "zurich",
-    "helsinki",
-    "copenhagen",
-    "reykjavik",
-    "oslo",
-    "stockholm",
-    "vienna",
-    "calgary"
-];
+// const cleanestCities = [
+//     "tokyo",
+//     "singapore",
+//     "zurich",
+//     "helsinki",
+//     "copenhagen",
+//     "reykjavik",
+//     "oslo",
+//     "stockholm",
+//     "vienna",
+//     "calgary"
+// ];
 
-let userInput = prompt("Enter a city name");
+// let userInput = prompt("Enter a city name");
 
-let isFound = false
+// let isFound = false
 
 
-for(let i = 0; i <= cleanestCities.length; i++){
-    if(userInput === cleanestCities[i]){
-        isFound = true;
-        break;
-    } else{
-        isFound = false
-    }
-}
+// for(let i = 0; i <= cleanestCities.length; i++){
+//     if(userInput === cleanestCities[i]){
+//         isFound = true;
+//         break;
+//     } else{
+//         isFound = false
+//     }
+// }
 
-if(isFound){
-    console.log("Yes Your City is in the list")
-}else{
-    console.log("Your City is not in the List")
-}
+// if(isFound){
+//     console.log("Yes Your City is in the list")
+// }else{
+//     console.log("Your City is not in the List")
+// }
+
+
+
+// While Loop
+// let i = 0
+// while (i <= 5) {
+//     console.log(i)
+
+//     i++
+// }
+
+
+let result = 0;
+
+let i = 0;
+
+do {
+    i = i+1  //i++
+    result = result+1 
+} while (i<5);
+
+console.log(result)
+
+
