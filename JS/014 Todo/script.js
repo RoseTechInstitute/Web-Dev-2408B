c66a731c45094810d1df7687a2f5002c288e09fc
RafayMemon01 <rafayrashid200611@gmail.com>
2025-01-09 17:51:24 +0500

    todos added

--- JS/014 Todo/script.js
+++ JS/014 Todo/script.js
@@ -1,1 +1,10 @@
-document.getElementById
+let todos = []
+
+
+document.getElementById('todoForm').addEventListener("submit",(e) =>{
+    e.preventDefault()
+    let userTodo = document.getElementById("userInput").value
+    todos.push(userTodo)
+
+    console.log(todos)
+});
