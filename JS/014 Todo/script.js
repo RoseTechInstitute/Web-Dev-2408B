2b35833c96aa835af641a87a0d1a0bff4cf1607d
RafayMemon01 <rafayrashid200611@gmail.com>
2025-01-14 18:05:50 +0500

    todo done

--- JS/014 Todo/script.js
+++ JS/014 Todo/script.js
@@ -5,6 +5,16 @@ document.getElementById('todoForm').addEventListener("submit",(e) =>{
     e.preventDefault()
     let userTodo = document.getElementById("userInput").value
     todos.push(userTodo)
-
+    document.getElementById("list").innerHTML = ""
+    showTodos(todos)
     console.log(todos)
 });
+
+
+function showTodos(todos) {
+
+    todos.forEach((todo)=>{
+        document.getElementById("list").innerHTML += `<li>${todo}</li>`
+    })
+    
+}
